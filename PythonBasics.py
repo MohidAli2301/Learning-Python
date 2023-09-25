78e854315380eb34d8ca22267f2188988dba31f4
MohidAli2301 <145489365+MohidAli2301@users.noreply.github.com>
2023-09-24 23:55:33 -0400

    Add files via upload

---
+++ PythonBasics.py
@@ -0,0 +1,32 @@
+#Learning Python!
+print ('hello there')
+print ("We are going to learn the basics of Python !!")
+
+#With \ slashes we can use special characters like \ "" '' within a print line. 
+#\n \t have special functions which do: next line, tab
+print ('I said \n \'\tget to work!!!!\' ')
+
+#Next is basic variables and their notation.
+#All variables must begin with a lowercase letter. In the case for Constants, a single capital letter will work
+a=5
+b="2023"
+c=4.3232
+CONSTANT = 55
+Also_Constant = "This is constant and cant be changed" 
+print(a,b,c)
+print (CONSTANT, Also_Constant)
+#Note how you are not required to state their type initially, however if you wish to change their type you can.
+float (a)
+int (b)
+str (c)
+print (a,b,c)
+
+#Variable working examples:
+width=3
+height=8
+rectangle_area= width*height
+triangle_area =(width*height)/2
+print(width*height)
+print((width*height)/2)
+print (rectangle_area)
+print (triangle_area)
