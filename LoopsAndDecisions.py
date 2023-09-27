93e7547fea02d9f3a9018f5d68f581a1f4c8762b
MohidAli2301 <145489365+MohidAli2301@users.noreply.github.com>
2023-09-27 12:20:49 -0400

    Update LoopsAndDecisions.py

--- LoopsAndDecisions.py
+++ LoopsAndDecisions.py
@@ -27,4 +27,28 @@ else:
   print("say this when first is false")
 #-----------------------------------------------------------------------------------------#
 #Loops with for and while
+for count in range (10,-1,-1): #Start at 10, count till -1, decrease by 1
+  print (count)
+for x in range (1,6): #Start at 1, until 6, default increment == 1
+     print (x)
+#Note: works in non-inclusive actions, if you want to count from 1-5 you must use 1,6
+
+#While loops are very simple, as they only run if a condition is true
+yes = True
+while(yes):
+  for x in range (1,6): #Start at 1, until 6, default increment == 1
+    print ("Hehe", x)
+  yes = False #-- comment this line to test infinite loop!
+#-----------------------------------------------------------------------------------------#
+#With both conditional statements and looping operators, we can implement and, or, as well as not
+yes = True
+no = False
+if not(no):
+  print("If invert no is equal True then this will work")
+  while (yes and yes):
+    print ("If BOTH are True this will work")
+    if yes or no == False:
+      print ("If EITHER is FALSE, then this will work")
+      yes = False
+      no = True
 
